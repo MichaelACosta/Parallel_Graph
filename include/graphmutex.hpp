cc493adf471e66022748f21c2f6a8b5cf19bce19
Michael Costa <macosta@inf.ufpel.edu.br>
2018-05-24 09:36:14 -0300

    graphmutex: refactored code removing comments
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- include/graphmutex.hpp
+++ include/graphmutex.hpp
@@ -4,28 +4,22 @@
 #include <graph.hpp>
 using namespace std;
 
-
 //As definições do grafo são herdadas da classe Graph
-
 class GraphMutex : public Graph {
 protected:
 	int vetor[size][size];
-	int flag[size]; //bool
+	int flag[size];
 
 public:
 	GraphMutex();
-
 	virtual void insertNode(int node);
 	virtual void deletNode(int node);
 	virtual bool testNode(int node) const;
-
 	virtual void changeEdge(int l, int c);
 	virtual void increaseEdge(int l, int c);
 	virtual void decreaseEdge(int l, int c);
 	virtual int getVal(int l, int c) const;
-
 	~GraphMutex();
-
 };
 
 #endif
--- src/graphmutex.cpp
+++ src/graphmutex.cpp
@@ -6,7 +6,6 @@
 
 mutex mgraph[size][size], mflag[size], mset;
 
-
 GraphMutex::GraphMutex(){
 	int i, c;
 	mset.lock();
@@ -19,15 +18,12 @@ GraphMutex::GraphMutex(){
 		}
 	}
 	mset.unlock();
-//	cout << "Grafo criado.\n";
 }
 
 void GraphMutex::insertNode(int node){
 	mflag[node].lock();
 	if(!flag[node]){
-		//mflag.lock();
 		flag[node]=1;
-		//mflag.unlock();
 	}
 	mflag[node].unlock();
 }
@@ -35,9 +31,7 @@ void GraphMutex::insertNode(int node){
 void GraphMutex::deletNode(int node){
 	mflag[node].lock();
 	if(flag[node]){
-		//mflag.lock();
 		flag[node]=0;
-		//mflag.unlock();
 	}
 	mflag[node].unlock();
 }
@@ -55,19 +49,13 @@ bool GraphMutex::testNode(int node)const{
 	return ret;
 }
 
-
 void GraphMutex::changeEdge(int l, int c){
 	if (l!=c){
-
-
 		this->insertNode(l);
 		this->insertNode(c);
-
-		//medge.lock();
 		mgraph[l][c].lock();
 		vetor[l][c]=1;
 		mgraph[l][c].unlock();
-		//medge.unlock();
 	}
 }
 
@@ -75,16 +63,11 @@ void GraphMutex::increaseEdge(int l, int c){
 	if (l!=c){
 		mgraph[l][c].lock();
 		if(vetor[l][c]){
-			//mset.lock();
-			//mgraph.lock();
 			vetor[l][c]++;
-			//mset.unlock();
-			//mgraph.unlock();
 		}
 		else{
 			this->insertNode(l);
 			this->insertNode(c);
-
 			vetor[l][c]=1;
 		}
 		mgraph[l][c].unlock();
@@ -95,11 +78,8 @@ void GraphMutex::decreaseEdge(int l, int c){
 	if (l!=c){
 		mgraph[l][c].lock();
 		if(vetor[l][c]){
-			//mgraph.lock();
 			vetor[l][c]--;
-			//mgraph.unlock();
 		}
-
 		if(!vetor[l][c]){
 			if(this->testNode(l) && this->testNode(c)){
 				int i, flagl=0, flagc=0;
@@ -112,7 +92,6 @@ void GraphMutex::decreaseEdge(int l, int c){
 						if(vetor[l][i] || vetor[i][l]){
 							flagl=1;
 						}
-
 						if(vetor[c][i] || vetor[i][c]){
 							flagc=1;
 						}
@@ -136,19 +115,10 @@ void GraphMutex::decreaseEdge(int l, int c){
 
 int GraphMutex::getVal(int l, int c) const{
 	int val=0;
-	//mget.lock();
 	mgraph[l][c].lock();
-	
 	val=vetor[l][c];
-	
-	//mget.unlock();
 	mgraph[l][c].unlock();
 	return val;
 }
 
-
-GraphMutex::~GraphMutex(){
-//	delete []vetor;
-//	delete []flag;
-//	cout << "Destrutor executado.\n";
-}
+GraphMutex::~GraphMutex(){}
